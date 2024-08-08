e904e028e127a32b7bb8ab0792eaeb2a27fb087e
Abdul Hadi <Abdulhadijatoi@gmail.com>
2024-08-09 02:22:48 +0400

    update

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -9,7 +9,7 @@ class PDFController extends Controller {
 
     public function generatePDF() {
         $data = [];
-        $pdf = PDF::loadView('pdf.quotation-form', $data);
+        $pdf = PDF::loadView('pdf-layout.quotation-form', $data);
         return $pdf->stream('quotation_'.now().'.pdf');
     }
 }
