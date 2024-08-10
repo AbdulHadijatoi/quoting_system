9b79956f63aeb8bb4097a68ac17e5378083b6f28
Abdul Hadi <Abdulhadijatoi@gmail.com>
2024-08-10 23:53:26 +0400

    added more migrations to fix database

--- app/Models/QuoteDetail.php
+++ app/Models/QuoteDetail.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class QuoteDetail extends Model
 {
     use HasFactory;
+
+    public function shippingQuote(){
+        return $this->belongsTo(ShippingQuote::class,'shipping_quote_id');
+    }
 }
--- app/Models/ShippingQuote.php
+++ app/Models/ShippingQuote.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class ShippingQuote extends Model
 {
     use HasFactory;
+
+    public function quoteDetails(){
+        return $this->hasOne(QuoteDetail::class, 'shipping_quote_id');
+    }
 }
