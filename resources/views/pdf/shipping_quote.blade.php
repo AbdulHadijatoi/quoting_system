724374ca8c5329f2fce7c6554ca3d5e37cd33c49
Abdul Hadi <Abdulhadijatoi@gmail.com>
2024-08-15 11:01:50 +0400

    update

--- resources/views/pdf/shipping_quote.blade.php
+++ resources/views/pdf/shipping_quote.blade.php
@@ -633,35 +633,35 @@
 
                                 <div class="text-small mb-5">
                                     <span class="w-70 d-inline-block">Asesoria en seguro</span>
-                                    <span class="w-25 d-inline-block text-center">${{ $seguro_carga }}</span>
+                                    <span class="w-25 d-inline-block text-center">${{ number_format($seguro_carga, 2) }}</span>
                                 </div>
                                 <div class="text-small mb-5">
                                     <span class="w-70 d-inline-block">Handling</span>
-                                    <span class="w-25 d-inline-block text-center">${{ $handling }}</span>
+                                    <span class="w-25 d-inline-block text-center">${{ number_format($handling, 2) }}</span>
                                 </div>
                                 <div class="text-small mb-5">
                                     <span class="w-70 d-inline-block">Descarga x tn.</span>
-                                    <span class="w-25 d-inline-block text-center">${{ $descarga_tn }}</span>
+                                    <span class="w-25 d-inline-block text-center">${{ number_format($descarga_tn, 2) }}</span>
                                 </div>
                                 <div class="text-small mb-5">
                                     <span class="w-70 d-inline-block">Visto Bueno</span>
-                                    <span class="w-25 d-inline-block text-center">${{ $visto_bueno }}</span>
+                                    <span class="w-25 d-inline-block text-center">${{ number_format($visto_bueno, 2) }}</span>
                                 </div>
                                 <div class="text-small mb-5">
                                     <span class="w-70 d-inline-block">Transporte interno</span>
-                                    <span class="w-25 d-inline-block text-center">${{ $shipping_amount }}</span>
+                                    <span class="w-25 d-inline-block text-center">${{ number_format($shipping_amount, 2) }}</span>
                                 </div>
                                 <div class="text-small mb-5">
                                     <span class="w-70 d-inline-block">Declaracion de aduana</span>
-                                    <span class="w-25 d-inline-block text-center">${{ $declaracion_aduanera }}</span>
+                                    <span class="w-25 d-inline-block text-center">${{ number_format($declaracion_aduanera, 2) }}</span>
                                 </div>
                                 <div class="text-small mb-5">
                                     <span class="w-70 d-inline-block">Gasto operativo</span>
-                                    <span class="w-25 d-inline-block text-center">${{ $almacenaje }}</span>
+                                    <span class="w-25 d-inline-block text-center">${{ number_format($almacenaje, 2) }}</span>
                                 </div>
                                 <div class="text-small">
                                     <span class="w-70 d-inline-block">Almacenaje aprox. canal verde</span>
-                                    <span class="w-25 d-inline-block text-center">${{ $gasto_operativo }}</span>
+                                    <span class="w-25 d-inline-block text-center">${{ number_format($gasto_operativo, 2) }}</span>
                                 </div>
 
                                 <div class="text-right w-90">
