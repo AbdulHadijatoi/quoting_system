35ca396c3461a2761526bc9aa505f71912ea5dd1
Abdul Hadi <Abdulhadijatoi@gmail.com>
2024-08-23 00:59:23 +0400

    added email view file

---
+++ resources/views/emails/quote.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your Shipping Quote</title>
+</head>
+<body>
+    <h1>Hello, {{ $data['name'] }}</h1>
+
+    <p>Thank you for requesting a shipping quote. Please find your quote attached to this email.</p>
+
+    <p>If you have any questions or need further assistance, feel free to reply to this email.</p>
+
+    <p>Best regards,<br>
+    Your Company Name</p>
+</body>
+</html>
